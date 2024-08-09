35cf3f7c175317c964908a63f00beb5b1fedc180
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2024-08-08 22:24:10 -0300

    Update ViewPrimeiraTela.java

--- ViewPrimeiraTela.java
+++ ViewPrimeiraTela.java
@@ -27,60 +27,89 @@ public class ViewPrimeiraTela extends javax.swing.JFrame {
     private void initComponents() {
 
         jTabbedPane1 = new javax.swing.JTabbedPane();
-        jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
-        jLabelNome = new javax.swing.JLabel();
-        jTextCampoNome = new javax.swing.JTextField();
+        jPanel3Login = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jTextFieldCampoNome = new javax.swing.JTextField();
+        jLabelSenha = new javax.swing.JLabel();
+        jLabelTitulo = new javax.swing.JLabel();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        jPanel4CriarConta = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        jLabel1.setText("Nome:");
+
+        jLabelSenha.setText("Senha:");
+
+        jLabelTitulo.setFont(new java.awt.Font("Arial Black", 1, 18)); // NOI18N
+        jLabelTitulo.setText("Informe seu login:");
+
+        javax.swing.GroupLayout jPanel3LoginLayout = new javax.swing.GroupLayout(jPanel3Login);
+        jPanel3Login.setLayout(jPanel3LoginLayout);
+        jPanel3LoginLayout.setHorizontalGroup(
+            jPanel3LoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3LoginLayout.createSequentialGroup()
+                .addGroup(jPanel3LoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel3LoginLayout.createSequentialGroup()
+                        .addGap(68, 68, 68)
+                        .addGroup(jPanel3LoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(jPanel3LoginLayout.createSequentialGroup()
+                                .addComponent(jLabelSenha)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jPasswordField1))
+                            .addGroup(jPanel3LoginLayout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jTextFieldCampoNome, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(jPanel3LoginLayout.createSequentialGroup()
+                        .addGap(95, 95, 95)
+                        .addComponent(jLabelTitulo)))
+                .addContainerGap(119, Short.MAX_VALUE))
+        );
+        jPanel3LoginLayout.setVerticalGroup(
+            jPanel3LoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3LoginLayout.createSequentialGroup()
+                .addGap(18, 18, 18)
+                .addComponent(jLabelTitulo)
+                .addGap(32, 32, 32)
+                .addGroup(jPanel3LoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextFieldCampoNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel3LoginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabelSenha)
+                    .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(121, Short.MAX_VALUE))
+        );
+
+        jLabelTitulo.getAccessibleContext().setAccessibleName("Informe seu login:");
+
+        jTabbedPane1.addTab("Login", jPanel3Login);
+
+        javax.swing.GroupLayout jPanel4CriarContaLayout = new javax.swing.GroupLayout(jPanel4CriarConta);
+        jPanel4CriarConta.setLayout(jPanel4CriarContaLayout);
+        jPanel4CriarContaLayout.setHorizontalGroup(
+            jPanel4CriarContaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 400, Short.MAX_VALUE)
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 265, Short.MAX_VALUE)
+        jPanel4CriarContaLayout.setVerticalGroup(
+            jPanel4CriarContaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 259, Short.MAX_VALUE)
         );
 
-        jTabbedPane1.addTab("tab1", jPanel1);
-
-        jLabelNome.setText("Nome:");
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabelNome)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextCampoNome, javax.swing.GroupLayout.PREFERRED_SIZE, 207, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(145, Short.MAX_VALUE))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(23, 23, 23)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabelNome)
-                    .addComponent(jTextCampoNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(220, Short.MAX_VALUE))
-        );
-
-        jTabbedPane1.addTab("tab2", jPanel2);
+        jTabbedPane1.addTab("Criar Conta", jPanel4CriarConta);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jTabbedPane1, javax.swing.GroupLayout.Alignment.TRAILING)
+            .addComponent(jTabbedPane1)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jTabbedPane1, javax.swing.GroupLayout.Alignment.TRAILING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(jTabbedPane1)
+                .addContainerGap())
         );
 
         pack();
@@ -122,10 +151,13 @@ public class ViewPrimeiraTela extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify                     
-    private javax.swing.JLabel jLabelNome;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabelSenha;
+    private javax.swing.JLabel jLabelTitulo;
+    private javax.swing.JPanel jPanel3Login;
+    private javax.swing.JPanel jPanel4CriarConta;
+    private javax.swing.JPasswordField jPasswordField1;
     private javax.swing.JTabbedPane jTabbedPane1;
-    private javax.swing.JTextField jTextCampoNome;
+    private javax.swing.JTextField jTextFieldCampoNome;
     // End of variables declaration                   
 }
